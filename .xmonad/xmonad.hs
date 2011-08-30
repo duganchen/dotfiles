aa4dc7471edf75210fa001ebe651d5b9f6a5c5c2
Dugan Chen <thedoogster@gmail.com>
2011-08-29 20:16:40 -0700

    Xmonad is dock-friendly now.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,9 +2,12 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 
 main =  xmonad $ ewmh defaultConfig
 	{
+		manageHook = manageDocks <+> manageHook defaultConfig,
+		layoutHook = avoidStruts $ layoutHook defaultConfig,
 		terminal = "urxvt"
 	}
 	`additionalKeysP`
