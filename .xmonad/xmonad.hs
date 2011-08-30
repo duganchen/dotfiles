bbaaef7d10284ed53f9a6eb4a296bfe01828aed7
Dugan Chen <thedoogster@gmail.com>
2011-08-29 21:04:11 -0700

    Shortcut key added for showing and hiding tint2.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,15 +3,18 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Config.Desktop
 
 main =  xmonad $ ewmh defaultConfig
 	{
-		manageHook = manageDocks <+> manageHook defaultConfig,
+		manageHook = manageDocks <+> manageHook desktopConfig,
 		layoutHook = avoidStruts $ layoutHook defaultConfig,
 		terminal = "urxvt"
 	}
 	`additionalKeysP`
 	[
-		("<Print>", spawn "scrot -e 'mv $f ~/Pictures/screenshots'")
+		("<Print>", spawn "scrot -e 'mv $f ~/Pictures/screenshots'"),
+		("M-n", sendMessage ToggleStruts)
 	]
+
 
