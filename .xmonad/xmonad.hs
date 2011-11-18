4a56c1e3ac47ce931fe085b7b67ebd925c09d5bb
Dugan Chen <dugan@ccnbikes.com>
2011-11-18 14:45:23 -0800

    Xmonad.hs no longer uses ewmhhints

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,26 +1,20 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Util.EZConfig
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Config.Desktop
-import XMonad.Layout.LayoutHints
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
-import XMonad.Hooks.ManageHelpers
-import XMonad.Actions.UpdatePointer
 
-conf = ewmh defaultConfig
-	{
-		modMask = mod4Mask
-		, manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll
-		[
-			isFullscreen --> doFullFloat
-		]
-		, layoutHook = smartBorders $ layoutHintsToCenter $ avoidStruts $ layoutHook defaultConfig 
-		, terminal = "urxvt"
-		, logHook = dynamicLog >> updatePointer (Relative 0.5 0.5)
-	}
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Actions.UpdatePointer
+import XMonad.Util.EZConfig
+import XMonad.Layout.NoBorders
+import XMonad.Layout.LayoutHints
+
+conf = defaultConfig
+              { manageHook = manageDocks <+> manageHook defaultConfig
+              , layoutHook = smartBorders $ layoutHintsToCenter $ avoidStruts  $  layoutHook defaultConfig
+			  , terminal = "urxvt"
+			  , logHook = dynamicLog >> updatePointer (Relative 0.5 0.5)
+			  , modMask = mod4Mask
+
+              }  
 	`additionalKeysP`
 	[
 		("<Print>", spawn "scrot -e 'mv $f ~/Pictures/screenshots'"),
@@ -28,8 +22,8 @@ conf = ewmh defaultConfig
 		("M-w", spawn "nitrogen --sort=rtime ~/Pictures/wallpaper")
 	]
 
-main = do
-	xmonad =<< xmobar conf
-		{
-			startupHook = startupHook conf >> setWMName "LG3D"
-		}
+main = do xmonad =<< xmobar conf
+
+
+			  
+
