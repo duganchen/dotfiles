dc3ca150d11987bf8664b81703b4f5437a6c0926
Dugan Chen <thedoogster@gmail.com>
2018-12-02 08:08:49 -0800

    Clean up cdmru.py a bit

--- .config/ranger/plugins/cdmru.py
+++ .config/ranger/plugins/cdmru.py
@@ -17,15 +17,15 @@ def hook_ready(fm):
                 mru = [pathlib.Path(directory.strip()) for directory in f.readlines()]
         mru = [directory for directory in mru if directory.is_dir()][:MRU_SIZE]
 
-        visitIndex = None
-
         try:
             visitIndex = mru.index(visit)
         except ValueError:
-            mru.insert(0, visit)
+            visitIndex = None
 
         if visitIndex is not None:
             mru.insert(0, mru.pop(visitIndex))
+        else:
+            mru.insert(0, visit)
 
         with conf.open(mode="w") as f:
             for directory in mru:
