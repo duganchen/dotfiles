5dc601d28a489bb3bad17b3cff677bc9270967cc
Dugan Chen <thedoogster@gmail.com>
2020-11-10 15:49:39 -0800

    Add the neovim treesitter Lua config

---
+++ .config/nvim/lua/treesitter/init.lua
@@ -0,0 +1,9 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true
+  }
+}
