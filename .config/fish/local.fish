5a077d81184d9a1cf861f744111294475940770d
Dugan Chen <thedoogster@gmail.com>
2022-04-22 15:55:42 -0700

    Add OS X support to FISH config

--- .config/fish/local.fish
+++ .config/fish/local.fish
@@ -11,9 +11,17 @@ export EDITOR='code --wait'
 export VISUAL='code --wait'
 
 # https://github.com/junegunn/fzf
+
+# Fedora
 if test -f /usr/share/fzf/shell/key-bindings.fish
     source /usr/share/fzf/shell/key-bindings.fish
 end
+
+# OS X
+if test -f /usr/local/opt/fzf/shell/key-bindings.fish
+    source /usr/share/fzf/shell/key-bindings.fish
+end
+
 fzf_key_bindings
 
 # https://github.com/gokcehan/lf
