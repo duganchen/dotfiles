4661b14b4bb8840f45ec3a1a5205b0fb98db1f7b
Dugan Chen <thedoogster@gmail.com>
2022-04-22 13:37:26 -0700

    FISH sets VISUAL and EDITOR to vscode

--- .config/fish/conf.d/local.fish
+++ .config/fish/conf.d/local.fish
@@ -2,6 +2,9 @@ set PATH $PATH ~/.local/bin
 zoxide init fish | source
 starship init fish | source
 
+export EDITOR='code --wait'
+export VISUAL='code --wait'
+
 if test -f /usr/share/fzf/shell/key-bindings.fish
     source /usr/share/fzf/shell/key-bindings.fish
 end
