a2ba6ca02ec4b0fbac3b5e0881abf01f766e2ef1
Dugan Chen <thedoogster@gmail.com>
2022-04-22 13:01:59 -0700

    Make FISH config flex for other distros

--- .config/fish/conf.d/local.fish
+++ .config/fish/conf.d/local.fish
@@ -1,5 +1,8 @@
 set PATH $PATH ~/.local/bin
 zoxide init fish | source
 starship init fish | source
-source /usr/share/fzf/shell/key-bindings.fish
+
+if test -f /usr/share/fzf/shell/key-bindings.fish
+    source /usr/share/fzf/shell/key-bindings.fish
+end
 fzf_key_bindings
