f8adf81a6f36cee679f7df9a57cfe226227d73f4
Dugan Chen <thedoogster@gmail.com>
2022-04-24 15:09:29 -0700

    Revert "Add FZF tab completion for FISH"
    This reverts commit c1acc9a382aa8cadb8a83d72f6ff61d02f8e1bee.

--- .config/fish/functions/clia.fish
+++
@@ -1,5 +0,0 @@
-function clia
-    read -l line
-    commandline -a $line
-    # commandline -a 
-end
--- .config/fish/functions/fuzzy_complete.fish
+++
@@ -1,4 +0,0 @@
-function fuzzy_complete
-    complete -C | sort -u | fzf --height 40% --multi --reverse -q (commandline -t) | cut --output-delimiter ' ' -f1 | sed s/-//g | clia
-    commandline -f end-of-line
-end
