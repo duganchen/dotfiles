be9e62f5270224a0f62ba85b5607546f9c4f22ee
Dugan Chen <thedoogster@gmail.com>
2023-07-07 14:42:51 -0700

    And that should start me off with xplr

---
+++ cfg/.config/xplr/init.lua
@@ -0,0 +1,14 @@
+version = '0.21.2'
+
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
+
+require("zentable").setup()
+require("icons").setup()
+require("extra-icons").setup()
+
+xplr.config.general.table.row.cols[2] = { format = "custom.icons_dtomvan_col_1" }
