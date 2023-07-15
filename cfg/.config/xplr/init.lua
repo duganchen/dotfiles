20874ba552935bcd68c2b3c9e66c3d9ccf35a718
Dugan Chen <thedoogster@gmail.com>
2023-07-14 19:11:29 -0700

    Just take index and perm columns off xplr

--- cfg/.config/xplr/init.lua
+++ cfg/.config/xplr/init.lua
@@ -7,11 +7,53 @@ package.path = home
 .. "/.config/xplr/plugins/?.lua;"
 .. package.path
 
-require("zentable").setup()
 require("icons").setup()
 require("extra-icons").setup()
 
 xplr.config.general.table.row.cols[2] = { format = "custom.icons_dtomvan_col_1" }
+
+-- Don't
+
+xplr.config.general.table.header.cols = {
+  -- { format = " index",            style = {} },
+  { format = "╭─── path", style = {} },
+  -- { format = "perm",              style = {} },
+  { format = "size",              style = {} },
+  { format = "modified",          style = {} },
+}
+
+xplr.config.general.table.row.cols = {
+  -- {
+  --   format = "builtin.fmt_general_table_row_cols_0",
+  --   style = {},
+  -- },
+  {
+    format = "builtin.fmt_general_table_row_cols_1",
+    style = {},
+  },
+  -- {
+  --   format = "builtin.fmt_general_table_row_cols_2",
+  --   style = {},
+  -- },
+  {
+    format = "builtin.fmt_general_table_row_cols_3",
+    style = {},
+  },
+  {
+    format = "builtin.fmt_general_table_row_cols_4",
+    style = {},
+  },
+}
+
+xplr.config.general.table.col_widths = {
+  -- { Percentage = 10 },
+  { Percentage = 70 },
+  -- { Percentage = 10 },
+  { Percentage = 10 },
+  { Percentage = 20 },
+}
+
+
 
 -- Uncomment this when I'm ready
 -- xplr.config.general.start_fifo = "/tmp/nnn.fifo"
