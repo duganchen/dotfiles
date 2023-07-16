f7ad6e3cc27aa5c4342686f4840e39d353b910f6
Dugan Chen <thedoogster@gmail.com>
2023-07-16 15:34:03 -0700

    The xplr previewer works good

--- cfg/.config/fish/functions/xplr.fish
+++ cfg/.config/fish/functions/xplr.fish
@@ -1,10 +1,9 @@
 function xplr
-    set --local pane_id (wezterm cli split-pane --right --percent 33 -- ~/.config/xplr/preview)
-    wezterm cli activate-pane
+    set --local pane_id (command wezterm cli split-pane --right --percent 33 -- ~/.config/xplr/preview)
+    command wezterm cli activate-pane
+
     set --local dir (command xplr --print-pwd-as-result $argv)
     if test -n "$dir"
         cd $dir
     end
-
-    # Looks like there's no need to kill the pane or the process in it.
 end
