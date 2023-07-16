2e0b1620c0111da41ee7a26e29ce3d9b4cd331e8
Dugan Chen <thedoogster@gmail.com>
2023-07-16 14:59:58 -0700

    xplr doesn't cd to /home/dugan when it shouldn't

--- cfg/.config/fish/functions/xplr.fish
+++ cfg/.config/fish/functions/xplr.fish
@@ -1,7 +1,10 @@
 function xplr
     set --local pane_id (wezterm cli split-pane --right --percent 33 -- ~/.config/xplr/preview)
     wezterm cli activate-pane
-    cd (command xplr --print-pwd-as-result $argv)
+    set --local dir (command xplr --print-pwd-as-result $argv)
+    if test -n "$dir"
+        cd $dir
+    end
 
     # Looks like there's no need to kill the pane or the process in it.
 end
