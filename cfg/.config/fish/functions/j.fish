b37279b866d4646ead61946858165e1e1a21a63e
Dugan Chen <thedoogster@gmail.com>
2023-07-25 16:32:30 -0700

    FISH doesn't need to add the current directory on jump

--- cfg/.config/fish/functions/j.fish
+++ cfg/.config/fish/functions/j.fish
@@ -1,13 +1,11 @@
 function j
     if count $argv > /dev/null
         if set --local result (command dirmarks list (pwd) ~/.dirmarks.json | command fzf --no-sort --select-1 --exit-0 --query $argv)
-            dirmarks add (pwd) ~/.dirmarks.json
             dirmarks add $result ~/.dirmarks.json
             cd $result
         end
     else
         if set --local result (command dirmarks list (pwd) ~/.dirmarks.json | command fzf --no-sort --select-1 --exit-0)
-            dirmarks add (pwd) ~/.dirmarks.json
             dirmarks add $result ~/.dirmarks.json
             cd $result
         end
