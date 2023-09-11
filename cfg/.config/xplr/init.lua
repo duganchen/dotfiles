ec3a22413eb99100b5356a01a92bd0766c29ec83
Dugan Chen <thedoogster@gmail.com>
2023-09-11 16:14:45 -0700

    More work on xplr layout

--- cfg/.config/xplr/init.lua
+++ cfg/.config/xplr/init.lua
@@ -7,7 +7,50 @@ require("icons").setup()
 require("extra-icons").setup()
 require("one-table-column").setup()
 
+-- The default key conflicts with my WezTerm setup. We change it to "c".
+local csw = require("context-switch")
+csw.setup({
+    key = "c",
+    layout = csw.builtin_layouts.without_help
+})
+
 xplr.config.general.initial_layout = "no_help_no_selection"
 
 -- This works with the FISH function that starts xplr...
 xplr.config.general.start_fifo = os.getenv("NNN_FIFO")
+
+xplr.fn.custom.render_context_num = function(ctx)
+    return {
+        CustomParagraph = {
+            ui = {
+                title = {
+                    format = "Ctx"
+                }
+            },
+            body = '  ' .. tostring(csw.get_current_context_num())
+        }
+    }
+end
+
+-- Add the context window to the left of the logs.
+
+local contextAndLayout = {
+    Horizontal = {
+        config = {
+            constraints = {{
+                Min = 7
+            }, {
+                Min = 1
+            }}
+        },
+        splits = {{
+            Dynamic = "custom.render_context_num"
+        }, "InputAndLogs"}
+    }
+}
+
+-- Notice that it's index 4 because we inserted the stats pane.
+xplr.config.layouts.builtin.default.Horizontal.splits[1].Vertical.splits[4] = contextAndLayout
+xplr.config.layouts.builtin.no_help.Horizontal.splits[1].Vertical.splits[4] = contextAndLayout
+xplr.config.layouts.builtin.no_selection.Horizontal.splits[1].Vertical.splits[4] = contextAndLayout
+xplr.config.layouts.builtin.no_help_no_selection.Vertical.splits[4] = contextAndLayout
