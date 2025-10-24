ddb2d0634dba27f51783bbcc885f19a38132f8dc
Dugan Chen <thedoogster@gmail.com>
2025-10-23 19:07:23 -0700

    Prettier 'l'

---
+++ cfg/.config/fish/functions/l.fish
@@ -0,0 +1,4 @@
+function l --wraps=eza --wraps='eza --color=always --group-directories-first --icons' --description 'alias l eza --color=always --group-directories-first --icons'
+  eza --color=always --group-directories-first --icons $argv
+        
+end
