c2228b4837178b49e8681b8701f4cbf71aa74349
Dugan Chen <dugan@node-1w7jr9qsft92yx1ea32vb3a9d.ipv6.telus.net>
2025-10-23 18:51:41 -0700

    Delete the eza abbreviations

--- cfg/.config/fish/conf.d/10-interactive.fish
+++ cfg/.config/fish/conf.d/10-interactive.fish
@@ -4,11 +4,6 @@ if status is-interactive
     fzf --fish | source
     zoxide init fish | source
 
-    abbr --add la eza --icons --all --group-directories-first
-    abbr --add l eza --icons --group-directories-first
-    abbr --add lla eza --icons --all --long --group-directories-first
-    abbr --add ll eza --icons --long --group-directories-first
-
     if test -f ~/apps/git-subrepo/.fish.rc
         source ~/apps/git-subrepo/.fish.rc
     end
