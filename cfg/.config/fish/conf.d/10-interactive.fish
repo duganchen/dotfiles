cb5e0c46e7a7ec6925c32ad3809ab555e57070eb
Dugan Chen <thedoogster@gmail.com>
2025-10-23 20:25:06 -0700

    FZF color scheme

--- cfg/.config/fish/conf.d/10-interactive.fish
+++ cfg/.config/fish/conf.d/10-interactive.fish
@@ -14,6 +14,14 @@ if status is-interactive
     set -x MANROFFOPT "-c"
     set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
+    # https://github.com/catppuccin/fzf/blob/main/themes/catppuccin-fzf-macchiato.fish
+    set -x FZF_DEFAULT_OPTS "\
+--color=bg+:#363A4F,spinner:#F4DBD6,hl:#ED8796 \
+--color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+--color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+--color=selected-bg:#494D64 \
+--color=border:#6E738D,label:#CAD3F5"
+
     fastfetch
 
     starship init fish | source
