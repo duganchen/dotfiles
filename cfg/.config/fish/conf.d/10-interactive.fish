237d3d1c1722a18d6cae8df52e237c6d6223ff1f
Dugan Chen <thedoogster@gmail.com>
2026-03-30 16:43:37 -0700

    I think I'm good enough with Helix now

--- cfg/.config/fish/conf.d/10-interactive.fish
+++ cfg/.config/fish/conf.d/10-interactive.fish
@@ -1,13 +1,18 @@
 # This file has stuff I know I'll always want. More host-specific stuff goes elsewhere.
 if status is-interactive
-	set -x LS_COLORS (vivid generate catppuccin-macchiato)
-	starship init fish | source
-	zoxide init fish | source
-	fzf --fish | source
+    set -x LS_COLORS (vivid generate catppuccin-macchiato)
+    starship init fish | source
+    zoxide init fish | source
+    fzf --fish | source
 
-	# Use bat for man pages
-	set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-	set -x MANROFFOPT "-c"
+    # Use bat for man pages
+    set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    set -x MANROFFOPT -c
 
-	fastfetch
+    set -x EDITOR hx
+    set -x VISUAL 'code --wait'
+
+    set -x BAT_THEME 'Catppuccin Macchiato'
+
+    fastfetch
 end
