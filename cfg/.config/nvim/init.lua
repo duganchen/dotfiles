aaff17e76a6623f5d5cccf03f2001227d7323c2a
Dugan Chen <thedoogster@gmail.com>
2026-04-19 12:30:46 -0700

    Neovim (0.12 and up) transparent catppuccin macchiato

---
+++ cfg/.config/nvim/init.lua
@@ -0,0 +1,4 @@
+vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+require('catppuccin').setup({transparent_background = true})
+vim.cmd.colorscheme "catppuccin-macchiato"
+
