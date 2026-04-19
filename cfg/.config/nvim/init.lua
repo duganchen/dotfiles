06f7a64d53a1e36a06c104923ac02840eb1fc6b6
Dugan Chen <thedoogster@gmail.com>
2026-04-19 16:40:28 -0700

    Set neovim up for bash

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -1,5 +1,8 @@
-vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+{ src = 'https://github.com/neovim/nvim-lspconfig' },}
 require('catppuccin').setup({transparent_background = true})
 vim.cmd.colorscheme "catppuccin-macchiato"
 
 vim.o.autocomplete = true
+
+vim.lsp.enable('bashls')
