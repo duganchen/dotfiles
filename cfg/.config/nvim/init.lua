ef87296698012b821eca652bf8ea565ea746203e
Dugan Chen <thedoogster@gmail.com>
2026-04-19 20:40:06 -0700

    I choose Arborist

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -1,6 +1,9 @@
 vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-{ src = 'https://github.com/neovim/nvim-lspconfig' },}
+{ src = 'https://github.com/neovim/nvim-lspconfig' },
+{ src = 'https://github.com/arborist-ts/arborist.nvim'}
+}
 require('catppuccin').setup({transparent_background = true})
+require('arborist').setup()
 vim.cmd.colorscheme "catppuccin-macchiato"
 
 vim.o.autocomplete = true
