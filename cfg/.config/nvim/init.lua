5a26057bae78781069da8f043c1614d70105ae4c
Dugan Chen <dugan@node-1w7jr9qsft92wko50yahn3btz.ipv6.telus.net>
2026-04-29 18:13:17 -0700

    Add back lualine

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -10,7 +10,9 @@ vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin"
 }
 require('catppuccin').setup({ transparent_background = true })
 
-require('nvim-treesitter').install({'lua', 'bash'})
+require('nvim-treesitter').install({ 'bash', 'lua', 'python' })
+
+require('lualine').setup()
 
 vim.cmd.colorscheme "catppuccin-macchiato"
 
@@ -19,53 +21,79 @@ vim.o.autocomplete = true
 vim.o.complete = "o,.,w,b,u"
 vim.o.completeopt = "fuzzy,menuone,noselect,popup"
 
-vim.lsp.enable({ 'lua_ls', 'bashls' })
+vim.lsp.enable({ 'lua_ls', 'bashls', 'ruff' })
 
 vim.lsp.config('lua_ls', {
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if
-        path ~= vim.fn.stdpath('config')
-        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-      then
-        return
-      end
-    end
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+			    path ~= vim.fn.stdpath('config')
+			    and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+			then
+				return
+			end
+		end
 
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most
-        -- likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Tell the language server how to find Lua modules same way as Neovim
-        -- (see `:h lua-module-load`)
-        path = {
-          'lua/?.lua',
-          'lua/?/init.lua',
-        },
-      },
-      -- Make the server aware of Neovim runtime files
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
-          vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
-          -- Depending on the usage, you might want to add additional paths
-          -- here.
-          -- '${3rd}/luv/library',
-          -- '${3rd}/busted/library',
-        },
-        -- Or pull in all of 'runtimepath'.
-        -- NOTE: this is a lot slower and will cause issues when working on
-        -- your own configuration.
-        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-        -- library = vim.api.nvim_get_runtime_file('', true),
-      },
-    })
-  end,
-  settings = {
-    Lua = {},
-  },
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most
+				-- likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+				-- Tell the language server how to find Lua modules same way as Neovim
+				-- (see `:h lua-module-load`)
+				path = {
+					'lua/?.lua',
+					'lua/?/init.lua',
+				},
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+					-- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+					vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+					-- Depending on the usage, you might want to add additional paths
+					-- here.
+					-- '${3rd}/luv/library',
+					-- '${3rd}/busted/library',
+				},
+				-- Or pull in all of 'runtimepath'.
+				-- NOTE: this is a lot slower and will cause issues when working on
+				-- your own configuration.
+				-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+				-- library = vim.api.nvim_get_runtime_file('', true),
+			},
+		})
+	end,
+	settings = {
+		Lua = {},
+	},
 })
+
+-- copy and paste from 
+-- https://dotfiles.substack.com/p/native-lsp-in-neovim-012
+vim.diagnostic.config({
+	severity_sort = true,
+	update_in_insert = false,
+	float = {
+		border = 'rounded',
+		source = 'if_many',
+	},
+	underline = true,
+	virtual_text = {
+		spacing = 2,
+		source = 'if_many',
+		prefix = '●',
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = 'E',
+			[vim.diagnostic.severity.WARN] = 'W',
+			[vim.diagnostic.severity.INFO] = 'I',
+			[vim.diagnostic.severity.HINT] = 'H',
+		},
+	},
+})
+
