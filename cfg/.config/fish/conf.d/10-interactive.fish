4c050c87aceaf8f5df24a29b626baa2cf6c32050
Dugan Chen <dugan@node-1w7jr9qsft92wko50yahn3btz.ipv6.telus.net>
2026-05-13 15:43:30 -0700

    Update FISH config

--- cfg/.config/fish/conf.d/10-interactive.fish
+++ cfg/.config/fish/conf.d/10-interactive.fish
@@ -1,15 +1,28 @@
 # This file has stuff I know I'll always want. More host-specific stuff goes elsewhere.
 if status is-interactive
-    set -x LS_COLORS (vivid generate catppuccin-macchiato)
-    starship init fish | source
-    fzf --fish | source
+    if command -v vivid >/dev/null
+        set -x LS_COLORS (vivid generate catppuccin-macchiato)
+    end
+
+    if command -v starship >/dev/null
+        starship init fish | source
+    end
+
+    if command -v fzf >/dev/null
+        fzf --fish | source
+    end
 
     # Use bat for man pages
     set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
     set -x MANROFFOPT -c
 
-    set -x EDITOR hx
-    set -x VISUAL 'code --wait'
+    if command -v nvim >/dev/null
+        set -x EDITOR nvim
+    end
+
+    if command -v code >/dev/null
+        set -x VISUAL 'code --wait'
+    end
 
     set -x BAT_THEME 'Catppuccin Macchiato'
 
