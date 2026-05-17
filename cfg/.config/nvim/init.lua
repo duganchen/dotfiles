cedb0ae7086e753c73c1eb4d920ccbe42c434e47
Dugan Chen <dugan@node-1w7jr9qsft92y1md2f1zwqm17.ipv6.telus.net>
2026-05-16 21:15:48 -0700

    Decided I want to show documentation

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -98,7 +98,10 @@ cmp.setup({
 		["<Tab>"] = { "select_next", "fallback" },
 		["<S-Tab>"] = { "select_prev", "fallback" },
 	},
-	completion = { list = { selection = { preselect = false, auto_insert = true } } }
+	completion = {
+		list = { selection = { preselect = false, auto_insert = true } },
+		documentation = { auto_show = true, auto_show_delay_ms = 500 }
+	}
 })
 
 -- This should use TreeSitter for folding?
