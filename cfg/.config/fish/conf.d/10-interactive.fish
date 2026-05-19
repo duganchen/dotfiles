6dee20dad6961b57da5540d299eaddfa3f4e9cf9
Dugan Chen <thedoogster@gmail.com>
2026-05-19 15:40:39 -0700

    Catppuccin FZF

--- cfg/.config/fish/conf.d/10-interactive.fish
+++ cfg/.config/fish/conf.d/10-interactive.fish
@@ -31,5 +31,12 @@ if status is-interactive
         fnm env --use-on-cd --shell fish | source
     end
 
+    # https://github.com/catppuccin/fzf
+    set -x FZF_DEFAULT_OPTS "\
+--color=bg+:#363A4F,spinner:#F4DBD6,hl:#ED8796 \
+--color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+--color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+--color=selected-bg:#494D64 \
+--color=border:#6E738D,label:#CAD3F5"
     fastfetch
 end
