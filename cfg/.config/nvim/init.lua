05848a6e8354fecf703daea52e6e7b4aafca4b96
Dugan Chen <dugan@node-1w7jr9qsft92y1md2f1zwqm17.ipv6.telus.net>
2026-05-23 13:22:56 -0700

    Add git plugins to neovim

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -40,7 +40,11 @@ vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin"
 	'git@github.com:mason-org/mason.nvim.git',
 	'git@github.com:nvim-treesitter/nvim-treesitter-textobjects.git',
 	'git@github.com:saghen/blink.cmp.git',
-	'git@github.com:saghen/blink.lib.git'
+	'git@github.com:saghen/blink.lib.git',
+
+	-- Shall we use both?
+	'git@github.com:lewis6991/gitsigns.nvim.git',
+	'git@github.com:tpope/vim-fugitive.git'
 
 }
 require('catppuccin').setup({ transparent_background = true })
@@ -329,44 +333,43 @@ end)
 -- mini.clue from the README
 local miniclue = require('mini.clue')
 miniclue.setup({
-  triggers = {
-    -- Leader triggers
-    { mode = { 'n', 'x' }, keys = '<Leader>' },
+	triggers = {
+		-- Leader triggers
+		{ mode = { 'n', 'x' }, keys = '<Leader>' },
 
-    -- `[` and `]` keys
-    { mode = 'n', keys = '[' },
-    { mode = 'n', keys = ']' },
+		-- `[` and `]` keys
+		{ mode = 'n',          keys = '[' },
+		{ mode = 'n',          keys = ']' },
 
-    -- Built-in completion
-    { mode = 'i', keys = '<C-x>' },
+		-- Built-in completion
+		{ mode = 'i',          keys = '<C-x>' },
 
-    -- `g` key
-    { mode = { 'n', 'x' }, keys = 'g' },
+		-- `g` key
+		{ mode = { 'n', 'x' }, keys = 'g' },
 
-    -- Marks
-    { mode = { 'n', 'x' }, keys = "'" },
-    { mode = { 'n', 'x' }, keys = '`' },
+		-- Marks
+		{ mode = { 'n', 'x' }, keys = "'" },
+		{ mode = { 'n', 'x' }, keys = '`' },
 
-    -- Registers
-    { mode = { 'n', 'x' }, keys = '"' },
-    { mode = { 'i', 'c' }, keys = '<C-r>' },
+		-- Registers
+		{ mode = { 'n', 'x' }, keys = '"' },
+		{ mode = { 'i', 'c' }, keys = '<C-r>' },
 
-    -- Window commands
-    { mode = 'n', keys = '<C-w>' },
+		-- Window commands
+		{ mode = 'n',          keys = '<C-w>' },
 
-    -- `z` key
-    { mode = { 'n', 'x' }, keys = 'z' },
-  },
+		-- `z` key
+		{ mode = { 'n', 'x' }, keys = 'z' },
+	},
 
-  clues = {
-    -- Enhance this by adding descriptions for <Leader> mapping groups
-    miniclue.gen_clues.square_brackets(),
-    miniclue.gen_clues.builtin_completion(),
-    miniclue.gen_clues.g(),
-    miniclue.gen_clues.marks(),
-    miniclue.gen_clues.registers(),
-    miniclue.gen_clues.windows(),
-    miniclue.gen_clues.z(),
-  },
+	clues = {
+		-- Enhance this by adding descriptions for <Leader> mapping groups
+		miniclue.gen_clues.square_brackets(),
+		miniclue.gen_clues.builtin_completion(),
+		miniclue.gen_clues.g(),
+		miniclue.gen_clues.marks(),
+		miniclue.gen_clues.registers(),
+		miniclue.gen_clues.windows(),
+		miniclue.gen_clues.z(),
+	},
 })
-
