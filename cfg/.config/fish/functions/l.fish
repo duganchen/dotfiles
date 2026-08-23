c35fa7dbcd3da00f76e92a8fba7076c884b6db18
Dugan Chen <dugan@node-1w7jr9phluij91myoe8gc4jrw.ipv6.telus.net>
2026-08-23 14:12:38 -0700

    This is necessary for wildcards

--- cfg/.config/fish/functions/l.fish
+++ cfg/.config/fish/functions/l.fish
@@ -1,4 +1,3 @@
 function l --wraps=eza --wraps='eza --color=always --group-directories-first --icons' --description 'alias l eza --color=always --group-directories-first --icons'
-  eza --color=always --group-directories-first --icons $argv
-        
+    eza --color=auto --group-directories-first --icons=auto $argv
 end
