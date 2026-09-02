3ffff979931fb1d60f1ad75f3b31a58fcfe92f35
Dugan Chen <thedoogster@gmail.com>
2026-09-02 09:56:38 -0700

    Comment

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -52,6 +52,7 @@ vim.api.nvim_create_autocmd('PackChanged', {
 require('catppuccin').setup({ transparent_background = true })
 
 require('mini.ai').setup()
+-- Note: the old gx is now gX
 require('mini.operators').setup()
 require('mini.basics').setup()
 require('mini.surround').setup()
