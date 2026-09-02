fbc52e89bee394b431e7c98d40845fff1e6b444c
Dugan Chen <thedoogster@gmail.com>
2026-09-01 19:29:56 -0700

    That should cover the LazyVim explorers

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -108,13 +108,18 @@ require('mini.snippets').setup({
 require('lazydev').setup()
 require('mason').setup()
 
+function MiniOpenRoot()
+	MiniFiles.open(vim.lsp.get_clients({ bufnr = 0 })[1].root_dir)
+end
+
 -- Note that mini.basics has set the leader key to space
 -- Mostly using Kickstart's setup, which starts finders with "<space>" s.
 -- No jumplist search though. Telescope has it, but AFAIK mini.pick doesn't
 vim.keymap.set('n', '<leader>sb', MiniPick.builtin.buffers, { desc = '[S]earch [B]uffers' })
 vim.keymap.set('n', '<leader>sf', MiniPick.builtin.files, { desc = '[S]earch [F]iles' })
 -- vim.keymap.set('n', '<leader>e', MiniExtra.pickers.explorer, { desc = 'open the explorer picker' })
-vim.keymap.set('n', '<leader>e', MiniFiles.open, { desc = 'open the explorer picker' })
+vim.keymap.set('n', '<leader>E', MiniFiles.open, { desc = '[E]xplorer (cwd)' })
+vim.keymap.set('n', '<leader>e', MiniOpenRoot, { desc = '[e]xplorer (root dir)' })
 -- Or <leader>/
 vim.keymap.set('n', '<leader>sg', MiniPick.builtin.grep_live, { desc = '[S]earch by [G]rep' })
 
